0432e10e5eb1797fa874963c0162bfbe59d1a63c
Cesar Jeanroy <cesar.jeanroy@busbud.com>
2025-12-08 16:16:54 -0500

    exo 2.2

--- exercises/02-llm-fundamentals/02.02-usage/problem/main.ts
+++ exercises/02-llm-fundamentals/02.02-usage/problem/main.ts
@@ -10,7 +10,27 @@ for await (const chunk of output.textStream) {
   process.stdout.write(chunk);
 }
 
+const output2 = streamText({
+  model: google('gemini-2.5-flash-lite'),
+  prompt: `Which country makes the best pies? Answer in a single paragraph.`,
+});
+
+for await (const chunk of output2.textStream) {
+  process.stdout.write(chunk);
+}
+
 console.log(); // Empty log to separate the output from the usage
 
 // TODO: Print the usage to the console
-TODO;
+const [usage, totalUsage, usage2, totalUsage2] =
+  await Promise.all([
+    output.usage,
+    output.totalUsage,
+    output2.usage,
+    output2.totalUsage,
+  ]);
+
+console.log(usage);
+console.log(totalUsage);
+console.log(usage2);
+console.log(totalUsage2);
