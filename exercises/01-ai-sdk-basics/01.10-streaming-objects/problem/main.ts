31d71ab6648b84d4e47215f920217c649ca9e031
Cesar Jeanroy <cesar.jeanroy@busbud.com>
2025-12-08 15:57:27 -0500

    exo: 1.10

--- exercises/01-ai-sdk-basics/01.10-streaming-objects/problem/main.ts
+++ exercises/01-ai-sdk-basics/01.10-streaming-objects/problem/main.ts
@@ -1,12 +1,14 @@
 import { google } from '@ai-sdk/google';
-import { streamText } from 'ai';
+import { streamObject, streamText } from 'ai';
+import z from 'zod';
 
 const model = google('gemini-2.5-flash-lite');
+const prompt =
+  'Give me the first paragraph of a story about an imaginary planet.';
 
 const stream = streamText({
   model,
-  prompt:
-    'Give me the first paragraph of a story about an imaginary planet.',
+  prompt,
 });
 
 for await (const chunk of stream.textStream) {
@@ -21,7 +23,15 @@ const finalText = await stream.text;
 //   passing in the finalText as the story
 // - The schema, which should be an object with a facts property
 //   that is an array of strings
-const factsResult = TODO;
+const factsResult = streamObject({
+  model,
+  prompt: 'What are the facts about this story: ' + finalText,
+  schema: z
+    .object({
+      fact: z.array(z.string()),
+    })
+    .describe('should contain all the facts about a text'),
+});
 
 for await (const chunk of factsResult.partialObjectStream) {
   console.log(chunk);
